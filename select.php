9d6afed53342a9406112b4909cb01ec2b24776b4
professordeng <2248262060@qq.com>
2019-05-11 19:50:50 +0800

    读取关系表数据

--- select.php
+++ select.php
@@ -14,6 +14,8 @@ $sql =	'SELECT blog_id, blog_title,
 		FROM blog_table';
 
 mysqli_select_db( $conn, 'blog' );
+
+//1. mysqli_fetch_array 函数, 参数是 MYSQLI_ASSOC
 $retval = mysqli_query( $conn, $sql );
 if(! $retval )
 {
@@ -30,5 +32,43 @@ while($row = mysqli_fetch_array($retval, MYSQLI_ASSOC))
 	"</tr>";
 }
 echo '</table>';
+
+//2. mysqli_fetch_assoc 函数
+$retval = mysqli_query( $conn, $sql );
+if(! $retval )
+{
+	die('无法读取数据: ' . mysqli_error($conn));
+}
+echo '<h2>菜鸟教程 mysqli_fetch_assoc 测试<h2>';
+echo '<table border="1"><tr><td>教程 ID</td><td>标题</td><td>作者</td><td>提交日期</td></tr>';
+while($row = mysqli_fetch_assoc($retval))
+{
+	    echo "<tr><td> {$row['blog_id']}</td> ".
+		"<td>{$row['blog_title']} </td> ".
+		"<td>{$row['blog_author']} </td> ".
+		"<td>{$row['submission_date']} </td> ".
+		"</tr>";
+}
+echo '</table>';
+
+//3. mysqli_fetch_array 函数，参数是 MYSQLI_NUM
+$retval = mysqli_query( $conn, $sql );
+if(! $retval )
+{
+	die('无法读取数据: ' . mysqli_error($conn));
+}
+echo '<h2>菜鸟教程 mysqli_fetch_array 测试<h2>';
+echo '<table border="1"><tr><td>教程 ID</td><td>标题</td><td>作者</td><td>提交日期</td></tr>';
+while($row = mysqli_fetch_array($retval, MYSQLI_NUM))
+{
+	    echo "<tr><td> {$row[0]}</td> ".
+		"<td>{$row[1]} </td> ".
+		"<td>{$row[2]} </td> ".
+		"<td>{$row[3]} </td> ".
+		"</tr>";
+}
+echo '</table>';
+
+mysqli_free_result($retval);
 mysqli_close($conn);
 ?>
