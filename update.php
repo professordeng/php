46a32957d888473d4b223445cbbe35bef43ed64a
professordeng <2248262060@qq.com>
2019-05-15 19:45:34 +0800

    更新数据

---
+++ update.php
@@ -0,0 +1,24 @@
+<?php
+$dbhost = 'localhost';       // mysql服务器主机地址
+$dbuser = 'blog';            // mysql用户名
+$dbpass = '123456';          // mysql用户名密码
+$conn = mysqli_connect($dbhost, $dbuser, $dbpass);
+if(! $conn )
+{
+	die('连接失败: ' . mysqli_error($conn));
+}
+// 设置编码，防止中文乱码
+mysqli_query($conn , "set names utf8");
+$sql =	"update blog_table set blog_author = '品如' where blog_id = 2";
+
+mysqli_select_db( $conn, 'blog' );
+
+// update set 更新数据
+$retval = mysqli_query( $conn, $sql );
+if(! $retval )
+{
+	die('无法更新数据: ' . mysqli_error($conn));
+}
+echo '数据更新成功！';
+mysqli_close($conn);
+?>
